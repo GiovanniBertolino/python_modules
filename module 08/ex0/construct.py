5b9571f6e727b0fa129d2c5ec695c79b5b6277e1
giovanni <giovanni@MacBook-Air-de-Stephane.local>
2026-05-21 16:28:03 +0200

    correcting module 08

--- module 08/ex0/construct.py
+++ module 08/ex0/construct.py
@@ -12,7 +12,7 @@ def construct() -> None:
         print("WARNING: You're in the global environment!")
         print("The machines can see everything you install.\n")
         print("To enter the construct, run:\n")
-        print("python -m venv matrix_env")
+        print("python3 -m venv matrix_env")
         print("source matrix_env/bin/activate # On Unix")
         print(r"matrix_env\Scripts\activate # On Windows")
         print("\nThen run this program again.")
